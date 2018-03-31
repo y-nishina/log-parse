37ba501053bede15f3b6fdc3becd3b6973d6b5db
Yuta Nishina <nishina@xirapha.jp>
2018-03-31 18:09:10 +0900

    モック的な

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule LogParse.PageController do
   use LogParse.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    #TODO ログファイルを読み込んで下記ダミーデータの形に整形する
+
+    # ダミーデータ TODO（後で消す）
+    logs = [
+      %{date: "2018/01/01", controller: "AAAController", method: "XXMethod", elapsedTime: "111ms"},
+      %{date: "2018/02/02", controller: "BBBController", method: "YYMethod", elapsedTime: "222ms"},
+      %{date: "2018/03/03", controller: "CCCController", method: "ZZMethod", elapsedTime: "333ms"},
+    ]
+
+    render conn, "index.html", logs: logs
   end
 end
